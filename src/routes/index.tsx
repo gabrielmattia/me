ba19ac2486fb4b8ace41cf5a79bd7f4a42f99672
Wellington Gabriel <bielsmi03@gmail.com>
2026-02-06 11:49:56 -0300

    learning

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
+import LoveQuestion from "@/components/LoveQuestion";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
-	return " Hello World!";
+	return <LoveQuestion />;
 }
